98f3ad4170fa5e218d1f7e27b194d45e0c944466
DIVEN009 <sainidiven@gmail.com>
2025-04-01 14:36:02 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import ProductCard from './components/ProductCard';
-import './App.css';
+import React, { useState } from "react";
+import ProductCard from "./components/ProductCard";
+import "./App.css";
 
 const initialProducts = [
   {
@@ -29,15 +29,39 @@ const initialProducts = [
   }
 ];
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState(initialProducts);
 
- 
+  const handleRatingSubmit = (productId, newRating) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === productId
+          ? {
+              ...product,
+              avgRating:
+                (product.avgRating * product.totalRatings + newRating) /
+                (product.totalRatings + 1),
+              totalRatings: product.totalRatings + 1
+            }
+          : product
+      )
+    );
+  };
 
   return (
-    <div>
-     {/* code here */}
+    <div className="container">
+      <h1>Product Ratings</h1>
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            onRatingSubmit={handleRatingSubmit}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
